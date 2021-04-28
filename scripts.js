ee64cef1fbb69c872c02c3dde7a9ec1279e1f05a
Manav Misra <manavm@visionwebsoft.com>
2021-04-28 12:25:56 -0500

    :sparkles: Add solution

--- scripts.js
+++ scripts.js
@@ -1,1 +1,55 @@
+const catalog = [
+  {
+    category: 'Sporting Goods',
+    price: '$49.99',
+    stocked: true,
+    name: 'Football',
+  },
+  {
+    category: 'Sporting Goods',
+    price: '$9.99',
+    stocked: true,
+    name: 'Baseball',
+  },
+  {
+    category: 'Sporting Goods',
+    price: '$29.99',
+    stocked: false,
+    name: 'Basketball',
+  },
+  {
+    category: 'Electronics',
+    price: '$99.99',
+    stocked: true,
+    name: 'iPod Touch',
+  },
+  {
+    category: 'Electronics',
+    price: '$399.99',
+    stocked: false,
+    name: 'iPhone 5',
+  },
+  { category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7' },
+];
 
+const input = document.querySelector('input');
+const ul = document.createElement('ul');
+let maxPrice = null;
+
+function render() {
+  const filteredCatalog = maxPrice
+    ? catalog.filter(({ price }) => price.slice(1) < maxPrice)
+    : catalog;
+
+  ul.innerHTML = filteredCatalog
+    .map(({ name, price }) => `<li>${name} - ${price}</li>`)
+    .join('');
+}
+
+document.body.appendChild(ul);
+render();
+
+input.addEventListener('input', event => {
+  maxPrice = Number(event.target.value);
+  render();
+});
